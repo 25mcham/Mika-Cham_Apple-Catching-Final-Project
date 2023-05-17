213788bf902f9252f65054f4a1d11ea30af55a68
Mika Cham <25mcham@castilleja.org>
2023-05-16 21:40:08 -0700

    I submitted my proposal, wrote down the steps for the project, sketched out the game's visuals, and started coding the first step.

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import processing.core.PApplet;
+import java.util.ArrayList;
+public class Main extends PApplet{
+    public static Main app;
+    public Main(){
+        super();
+        app = this;
+    }
+
+    public void settings(){
+        size(1400,600);
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+        //drawPlayer();
+    }
+
+    public void drawPlayer(int x){
+
+    }
+}
