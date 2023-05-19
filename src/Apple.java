6af0f94e23e8f70ff212e0a5fb24434c4dc042ad
Mika Cham <25mcham@castilleja.org>
2023-05-19 14:12:14 -0700

    I worked on the arrowkey movement, and created the apple class, completed the temporary background.

---
+++ src/Apple.java
@@ -0,0 +1,18 @@
+public class Apple {
+    private int speed;
+    private boolean caught;
+    private boolean show;
+    private int xPos;
+
+    public Apple(int speed, boolean caught, boolean show, int xPos){
+        this.speed = speed;
+        this. caught = caught;
+        this.show = show;
+        this.xPos = xPos;
+    }
+
+    public void display(){
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import processing.core.PApplet;
 import java.util.ArrayList;
 public class Main extends PApplet{
+    private int currentXCoord = 700;
+    private int changeX = 0; //change in players location
     public static Main app;
+    //private boolean moving = false; //moving is true when the key is pressed but not released
     public Main(){
         super();
         app = this;
@@ -12,14 +15,42 @@ public class Main extends PApplet{
     }
 
     public void setup(){
-
+        background(154, 212, 237);
+        drawBackground();
     }
+
 
     public void draw(){
-        //drawPlayer();
+        noStroke();
     }
 
-    public void drawPlayer(int x){
+    public void updatePlayer(){
+        fill(154, 212, 237);
+        rect(currentXCoord, 300,currentXCoord +60, 600);// clearing out the background
 
+        currentXCoord += changeX;
+        fill(255,255,255);
+        rect(currentXCoord,300,currentXCoord+60,450);// draw character
+    }
+
+    public void drawBackground(){
+        fill(80, 150, 75);
+        ellipse(700,100,800,60);
+    }
+
+    public void keyPressed() {
+        //moving = true;
+        //while(moving)
+
+        if(key == 37){// if the key pressed is the left arrow
+            changeX = -10;
+        } else if(key == 38){ //right arrow is pressed
+            changeX = 10;
+        }
+        updatePlayer();
+    }
+
+    public void keyReleased(){
+        changeX = 0;
     }
 }
