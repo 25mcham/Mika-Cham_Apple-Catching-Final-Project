ae3b8d0a2addedc7ed7825f8dcf5536f14f2d121
Mika Cham <25mcham@castilleja.org>
2023-05-19 15:17:27 -0700

    I displayed the player and apples, instantiated the apple array, and worked on implementing the apple falling feature

--- src/Apple.java
+++ src/Apple.java
@@ -3,16 +3,34 @@ public class Apple {
     private boolean caught;
     private boolean show;
     private int xPos;
+    private int yPos;
 
-    public Apple(int speed, boolean caught, boolean show, int xPos){
+    public Apple(int speed, boolean caught, boolean show, int xPos, int yPos){
         this.speed = speed;
         this. caught = caught;
         this.show = show;
         this.xPos = xPos;
+        this.yPos = yPos;
     }
 
     public void display(){
-
+        Main.app.fill(232, 60, 26); //red
+        Main.app.ellipse(xPos,yPos,80,80);
     }
 
+    public int getxPos(){
+        return xPos;
+    }
+
+    public int getyPos(){
+        return yPos;
+    }
+
+    public void setyPos(int sety){
+        yPos = sety;
+    }
+
+    public void setxPos(int setx){
+        xPos = setx;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 import processing.core.PApplet;
 import java.util.ArrayList;
 public class Main extends PApplet{
-    private int currentXCoord = 700;
+    private int currentXCoord = 700; //left most x on player
     private int changeX = 0; //change in players location
     public static Main app;
+    private int numApples = 10; //total number of apples;
     //private boolean moving = false; //moving is true when the key is pressed but not released
+
+    private ArrayList<Apple> pomme = new ArrayList<Apple>();
     public Main(){
         super();
         app = this;
+    }
+
+    public static void main(String[] args){
+        PApplet.main("Main");
     }
 
     public void settings(){
@@ -17,25 +24,49 @@ public class Main extends PApplet{
     public void setup(){
         background(154, 212, 237);
         drawBackground();
+        updatePlayer();
+
+        for(int i = 0; i < numApples; i++){// instantiate arraylist
+            Apple a = new Apple(10,false,true,(int)(1400/ numApples*(i+0.5)),(int)(Math.random()*180+45));
+            pomme.add(a);
+        }
     }
 
 
     public void draw(){
         noStroke();
+
+        for(int i = 0; i < numApples; i++){
+            pomme.get(i).display();
+        }
+
+        Apple temp = pomme.get(0);
+
+        /*while(temp.getyPos() < 580){
+            temp.setyPos(temp.getyPos()-1);
+            temp.display();
+        }
+
+         */
     }
 
     public void updatePlayer(){
-        fill(154, 212, 237);
-        rect(currentXCoord, 300,currentXCoord +60, 600);// clearing out the background
+        fill(80, 150, 75); //grassgreen
+        rect(currentXCoord, 450,60, 120);// clearing out the background
 
         currentXCoord += changeX;
         fill(255,255,255);
-        rect(currentXCoord,300,currentXCoord+60,450);// draw character
+        rect(currentXCoord, 450,60, 120);// draw character
     }
 
     public void drawBackground(){
-        fill(80, 150, 75);
-        ellipse(700,100,800,60);
+        noStroke();
+        fill(80, 150, 75); //grassgreen
+        rect(0,400,2000,1000);//grass
+        fill(128, 100, 69);//trunkbrown
+        rect(620, 200,110,300); //tree trunk
+        fill(37, 89, 39);//leafgreen
+        ellipse(700,50,1500,500);// tree leaves
     }
 
     public void keyPressed() {
@@ -53,4 +84,6 @@ public class Main extends PApplet{
     public void keyReleased(){
         changeX = 0;
     }
+
+    public void fall(){}
 }
